a8874c45645a148a91a0dfab70f3cc6681da64e2
Leeeeeming <895148716@qq.com>
2021-02-06 14:30:59 +0800

    构建eureka-server模块

---
+++ ad-eureka/src/main/java/com.yahaha.ad/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.yahaha.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Auther LeeMZ
+ * @Date 2021/2/6
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
